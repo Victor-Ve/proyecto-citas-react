6668dbbf699bb862a0ed63a1be70887b96a361ee
victor_venegas <victort8012@gmail.com>
2020-08-13 17:44:42 -0400

    Agregando campos al formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 const Formulario = () => {
-  return <h1>Desde Formulario</h1>;
+  return (
+    <Fragment>
+      <h2>Crear cita</h2>
+      <form>
+        <label>Nombre Mascota</label>
+        <input
+          type='text'
+          name='mascota'
+          className='u-full-width'
+          placeholder='Nombre mascota'
+        />
+        <label>Nombre Dueño</label>
+        <input
+          type='text'
+          name='propietarios'
+          className='u-full-width'
+          placeholder='Nombre dueño de la mascota'
+        />
+        <label>Fecha</label>
+        <input
+          type='date'
+          name='fecha'
+          className='u-full-width'
+        />
+        <label>Hora</label>
+        <input
+          type='time'
+          name='hora'
+          className='u-full-width'
+        />
+        <label>Síntomas</label>
+        <textarea
+          className='u-full-width'
+          name='sintomas'></textarea>
+        <button
+          type='submit'
+          className='u-full-width button-primary'>
+          Agregar cita
+        </button>
+      </form>
+    </Fragment>
+  );
 };
 
 export default Formulario;
