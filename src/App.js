c55cde533899790c1888c4294b1555b6cf113890
victor_venegas <victort8012@gmail.com>
2020-08-13 20:30:44 -0400

    Usando useEffect para agregar las citas a el localStorage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import Formulario from "./components/Formulario";
 import Cita from "./components/Cita";
 
 function App() {
+  //citas en localStorage
+  let citasIniciales = JSON.parse(localStorage.getItem("citas"));
+  if (!citasIniciales) {
+    citasIniciales = [];
+  }
+
   //Arreglo de citas
-  const [citas, guardarCitas] = useState([]);
+  const [citas, guardarCitas] = useState(citasIniciales);
+
+  //Use Effect para realizar ciertas operaciones cuando el state cambia
+  useEffect(() => {
+    if (citasIniciales) {
+      localStorage.setItem("citas", JSON.stringify(citas));
+    } else {
+      localStorage.setItem("citas", JSON.stringify([]));
+    }
+  }, [citas]);
 
   //funcion que tomas las citas acuales y agrega la nueva
   const crearCita = (cita) => {
